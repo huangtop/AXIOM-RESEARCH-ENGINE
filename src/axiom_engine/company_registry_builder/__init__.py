623c7d09aead91066c1826cd7ea50fe32f670ade
huangtop <hphuang319@gmail.com>
2026-08-22 11:00:13 +0800

    refactor: retire legacy real-100 population pipeline

--- src/axiom_engine/company_registry_builder/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import RegistryBuildReport, build_real_100_registry, validate_real_100_registry
-
-__all__ = ["RegistryBuildReport", "build_real_100_registry", "validate_real_100_registry"]
--- src/axiom_engine/real_100_onboarding/__init__.py
+++
@@ -1,2 +0,0 @@
-from .core import load_cohort, onboarding_status, build_sec_registry_source
-__all__=['load_cohort','onboarding_status','build_sec_registry_source']
